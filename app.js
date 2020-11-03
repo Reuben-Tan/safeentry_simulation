3e7bfe8438287bcb2741d869ba8b946e27cc043b
Reuben Tan KunHan <rtankunhan@gmail.com>
2020-11-03 15:28:33 +0800

    updating surface

--- app.js
+++ app.js
@@ -54,4 +54,55 @@ function redrawWindow(){
 	window.clearInterval(simTimer); // clear the Timer
 	animationDelay = 550 - document.getElementById("slider1").value;
 	simTimer = window.setInterval(simStep, animationDelay); // call the function simStep every animationDelay milliseconds
+    //resize the drawing surface; remove all its contents; 
+	var drawsurface = document.getElementById("surface");
+	var creditselement = document.getElementById("credits");
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	var surfaceWidth =(w - 3*WINDOWBORDERSIZE);
+    var surfaceHeight= (h-creditselement.offsetHeight - 3*WINDOWBORDERSIZE);
+    
+    drawsurface.style.width = surfaceWidth+"px";
+	drawsurface.style.height = surfaceHeight+"px";
+	drawsurface.style.left = WINDOWBORDERSIZE/2+'px';
+	drawsurface.style.top = WINDOWBORDERSIZE/2+'px';
+	drawsurface.style.border = "thick solid #0000FF"; //The border is mainly for debugging; okay to remove it
+	drawsurface.innerHTML = ''; //This empties the contents of the drawing surface, like jQuery erase().
+	
+	// Compute the cellWidth and cellHeight, given the size of the drawing surface
+	numCols = maxCols;
+	cellWidth = surfaceWidth/numCols;
+	numRows = Math.ceil(surfaceHeight/cellWidth);
+	cellHeight = surfaceHeight/numRows;
+	
+	// In other functions we will access the drawing surface using the d3 library. 
+	//Here we set the global variable, surface, equal to the d3 selection of the drawing surface
+	surface = d3.select('#surface');
+	surface.selectAll('*').remove(); // we added this because setting the inner html to blank may not remove all svg elements
+	surface.style("font-size","100%");
+	// rebuild contents of the drawing surface
+	updateSurface();	
+};
+
+function updateDynamicAgents(){
+	// loop over all the agents and update their states
+	for (var patientIndex in patients){
+		updatePatient(patientIndex);
+	}
+	updateSurface();	
+}
+
+function simStep(){
+	//This function is called by a timer; if running, it executes one simulation step 
+	//The timing interval is set in the page initialization function near the top of this file
+	if (isRunning){ //the isRunning variable is toggled by toggleSimStep
+		// Increment current time (for computing statistics)
+		currentTime++;
+		// Sometimes new agents will be created in the following function
+		addDynamicAgents();
+		// In the next function we update each agent
+		updateDynamicAgents();
+		// Sometimes agents will be removed in the following function
+		removeDynamicAgents();
+	}
 }
